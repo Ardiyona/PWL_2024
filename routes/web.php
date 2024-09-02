a375b4da046bb0ee50915aa60f9fab0796b7d6ea
Dhiya Rakha Ardiyona <rakhaardiyona98@gmail.com>
2024-09-02 10:45:39 +0700

    Pertemuan2 - Praktikum1

--- routes/web.php
+++ routes/web.php
@@ -51,19 +51,3 @@ Route::get(
 $url = route('profile');
 // Generating Redirects...
 return redirect()->route('profile'); */
-   
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
-//Callback function
-// Route::get('/', function () {
-//     return view('welcome');
-// });
