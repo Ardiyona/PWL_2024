77af12b3ada1c35342cf8ff421a163cd209dcb98
Dhiya Rakha Ardiyona <rakhaardiyona98@gmail.com>
2024-09-02 10:40:40 +0700

    Praktikum 2

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,57 @@
 <?php
 
+use App\Http\Controllers\UserProfileController;
 use Illuminate\Support\Facades\Route;
 
+Route::get('/hello', function () {
+    return 'Hello World';
+});
+
+Route::get('/world', function () {
+    return 'World';
+});
+
+Route::get('/', function () {
+    return 'Selamat Datang';
+});
+
+Route::get('/about', function () {
+    return '2241760078';
+});
+
+/* Route::get('/user/{name}', function ($name) {
+    return 'Nama saya '.$name;
+}); */
+
+Route::get('/posts/{post}/comments/{comment}', function 
+($postId, $commentId) {
+ return 'Pos ke-'.$postId." Komentar ke-: ".$commentId;
+});
+
+Route::get('/articles/{id}', function ($id) {
+    return 'Halaman Artikel dengan ID '.$id;
+});
+
+/* Route::get('/user/{name?}', function ($name=null) {
+    return 'Nama saya '.$name;
+}); */
+
+Route::get('/user/{name?}', function ($name='John') {
+    return 'Nama saya '.$name;
+});
+
+/* Route::get('/user/profile', function () {
+    //
+   })->name('profile');
+Route::get(
+'/user/profile',
+    [UserProfileController::class, 'show']
+)->name('profile');
+// Generating URLs...
+$url = route('profile');
+// Generating Redirects...
+return redirect()->route('profile'); */
+   
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +63,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//Callback function
+// Route::get('/', function () {
+//     return view('welcome');
+// });
